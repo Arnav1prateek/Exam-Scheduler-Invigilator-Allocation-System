2af06e92d0d4315a9f074e921b39ef759388c04f
Arnav1prateek <arnav.p22@gmail.com>
2025-03-06 21:50:06 +0530

    Added faculty data

---
+++ LocalDB.session.sql
@@ -0,0 +1,11 @@
+INSERT INTO Faculty (faculty_id, name, email, phone_number, department, availability_status) VALUES
+('RA2311003010FA001', 'Dr. D Viji', 'vijid@srmist.edu.in', '9876543210', 'Computer Science', TRUE),
+('RA2311003010FA002', 'Dr. Mahendran K', 'mahik@srmist.edu.in', '9876543211', 'Electrical Engineering', TRUE),
+('RA2311003010FA003', 'Dr. Sudestna Pathak', 'sudhp@srmist.edu.in', '9876543212', 'Mechanical Engineering', TRUE),
+('RA2311003010FA004', 'Dr. Sindhuja M', 'sidhum@srmist.edu.in', '9876543213', 'Civil Engineering', TRUE),
+('RA2311003010FA005', 'Dr. Ramaprabha J', 'ramuj@srmist.edu.in', '9876543214', 'Chemical Engineering', TRUE),
+('RA2311003010FA006', 'Dr. Vivek Justus', 'vivuj@srmist.edu.in', '9876543215', 'Computer Science', TRUE),
+('RA2311003010FA007', 'Dr. Deepa R', 'deepuR@srmist.edu.in', '9876543216', 'Electrical Engineering', TRUE),
+('RA2311003010FA008', 'Dr. Dhanavati G', 'dhanuG@srmist.edu.in', '9876543217', 'Mechanical Engineering', TRUE),
+('RA2311003010FA009', 'Dr. Senthil Kumar', 'sentK@srmist.edu.in', '9876543218', 'Civil Engineering', TRUE),
+('RA2311003010FA010', 'Dr. Geeta G ', 'gg@srmist.edu.in', '9876543219', 'Chemical Engineering', TRUE);
